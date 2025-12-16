2a35a1f1942202d911b60994e64ebe0dcf6ab26b
Lucas Andrade <lucasilva108@gmail.com>
2025-12-15 23:09:14 -0300

    feat: display balance on the banner component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { BannerComponent } from "./banner/banner.component";
 import { FormNovaTransacaoComponent } from "./form-nova-transacao/form-nova-transacao.component";
+import { TipoTransacao, Transacao } from './modelos/transacao';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,26 @@ import { FormNovaTransacaoComponent } from "./form-nova-transacao/form-nova-tran
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'anybank';
+  transacoes = signal<Transacao[]>([]);
+
+  saldo = computed(() => {
+    return this.transacoes().reduce((acc, transacao) => {
+      switch (transacao.tipo) {
+        case TipoTransacao.DEPOSITO:
+          return acc + transacao.valor;
+        case TipoTransacao.SAQUE:
+          // if (acc < transacao.valor) {
+          //   alert('Saldo insuficiente. Saldo atual: R$ ' + acc);
+          //   return 0;
+          // }
+          return acc - transacao.valor;
+        default:
+          throw new Error('Tipo de transação não identificado.');
+      }
+    }, 0)
+  });
+
+  processarTransacao(transacao: Transacao) {
+    this.transacoes.update((listaAtual) => [transacao, ...listaAtual]);
+  }
 }
