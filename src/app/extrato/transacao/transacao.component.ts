6b8c884b964547d54903db2737521428a790a125
Lucas Andrade <lucasilva108@gmail.com>
2025-12-16 23:29:19 -0300

    feat: adding transacao's component

---
+++ src/app/extrato/transacao/transacao.component.ts
@@ -0,0 +1,13 @@
+import { Component, input } from '@angular/core';
+import { Transacao } from '../../modelos/transacao';
+import { CurrencyPipe } from '@angular/common';
+
+@Component({
+  selector: 'app-transacao',
+  imports: [CurrencyPipe],
+  templateUrl: './transacao.component.html',
+  styleUrl: './transacao.component.css'
+})
+export class TransacaoComponent {
+  transacao = input.required<Transacao>();
+}
